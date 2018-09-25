ab472f2a643dc93f554130f1510d5cc195345690
dextroxd <divyanshu171299@gmail.com>
2018-09-25 14:34:01 +0530

    Added new WordClass and new Layout

---
+++ app/src/main/java/Word.java
@@ -0,0 +1,22 @@
+public class Word
+{
+    private String Miwokwords;
+    private String Englishwords;
+    public  void set_miwok_Text(String text)
+    {
+        Miwokwords = text;
+    }
+    public  void set_english_Text(String text)
+    {
+        Englishwords = text;
+    }
+    public  String get_miwok_Text()
+    {
+        return (Miwokwords);
+    }
+
+    public String get_english_Text()
+    {
+        return Englishwords;
+    }
+}
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -28,7 +28,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Nine");
         words.add("Ten");
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, R.layout.list_item, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
